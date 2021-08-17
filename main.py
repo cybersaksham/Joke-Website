33a7ffa28c5526d5c8cac024d82efdfdf9613721
Saksham <2020UCP1801@mnit.ac.in>
2021-08-17 11:56:28 +0530

    html form

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import json
 
+# Global Variables
 with open("config.json") as f:
     data = json.load(f)
+with open("static/data.json") as f:
+    langData = json.load(f)
+    lang__, cat__ = langData["languages"], langData["categories"]
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", lang=lang__, cat=cat__)
+
 
 if __name__ == '__main__':
     app.run(debug=data["debug"])
